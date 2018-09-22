f7423ceb5a03dfbf5178ca466ce9a180e0697ebd
hamadou diallo <hmaadou_diallo@yahoo.com>
2018-09-22 15:22:17 -0400

    connection made

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const socket = require('socket.io')
 const app = express();
 const server = app.listen(3000, () => {
   console.log('listening on 3000')
 });
 
 app.use(express.static('public'))
+
+const io = socket(server)
+io.on('connection', (socket) => {
+  console.log('Connection made')
+})
